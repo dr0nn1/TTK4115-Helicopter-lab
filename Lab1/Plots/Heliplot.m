e9619170d3ed6bf24468baff29c90b430db0533b
dr0nn1 <peda_95@hotmail.com>
2020-09-22 17:06:28 +0200

    Finished with Lab 1

---
+++ Lab1/Plots/Heliplot.m
@@ -0,0 +1,39 @@
+clc
+clear
+close
+Data = load('pitch.mat');
+Data2 = load('pitch2.mat');
+Data3 = load('pitch3.mat');
+Data4 = load('pitch4.mat');
+Data5 = load('pitch5.mat');
+Data6 = load('pitch6.mat');
+Data7 = load('pitch7.mat');
+
+
+figure(1);
+plot(Data.ans(1,:), Data.ans(2,:),'k')
+
+hold on
+
+plot(Data.ans(1,:), Data.ans(3,:),'y')
+
+hold on
+plot(Data2.ans(1,:), Data2.ans(3,:),'m')
+
+hold on
+plot(Data3.ans(1,:), Data3.ans(3,:),'c')
+
+hold on
+plot(Data4.ans(1,:), Data4.ans(3,:),'r')
+
+hold on
+plot(Data5.ans(1,:), Data5.ans(3,:),'g')
+
+hold on
+plot(Data6.ans(1,:), Data6.ans(3,:))
+
+hold on
+plot(Data7.ans(1,:), Data7.ans(3,:),'b')
+
+
+legend('Step=0.6','Test 1','Test 2','Test 3','Test 4','Test 5','Test 6','Test 7');
---
+++ Lab1/init_heli_3_10.m
@@ -0,0 +1,42 @@
+% FOR HELICOPTER NR 3-10
+% This file contains the initialization for the helicopter assignment in
+% the course TTK4115. Run this file before you execute QuaRC_ -> Build 
+% to build the file heli_q8.mdl.
+
+% Oppdatert høsten 2006 av Jostein Bakkeheim
+% Oppdatert høsten 2008 av Arnfinn Aas Eielsen
+% Oppdatert høsten 2009 av Jonathan Ronen
+% Updated fall 2010, Dominik Breu
+% Updated fall 2013, Mark Haring
+% Updated spring 2015, Mark Haring
+
+
+%%%%%%%%%%% Calibration of the encoder and the hardware for the specific
+%%%%%%%%%%% helicopter
+Joystick_gain_x = 1;
+Joystick_gain_y = -1;
+
+
+%%%%%%%%%%% Physical constants
+g = 9.81; % gravitational constant [m/s^2]
+l_c = 0.46; % distance elevation axis to counterweight [m]
+l_h = 0.66; % distance elevation axis to helicopter head [m]
+l_p = 0.175; % distance pitch axis to motor [m]
+m_c = 1.92; % Counterweight mass [kg]
+m_p = 0.72; % Motor mass [kg]
+
+%%%%%%%%%%Caltulated variabels
+Vs_o = 7.743;
+Kf = 0.129;
+L1= Kf*l_p;
+K1 =(L1)/(2*m_p*(l_p)^2);
+
+%%%%%%%%%Choose desiered poles:
+Pole1= -1+2j;
+Pole2 = -1-2j;
+%%%%%%%%%
+Kpp = (Pole1*Pole2)/K1;
+Kpd = (-Pole1-Pole2)/K1;
+
+
+
