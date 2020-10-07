754adf97dfa46d623319030242c6dda83f3674cf
dr0nn1 <peda_95@hotmail.com>
2020-10-07 10:40:41 +0200

    finished lab 2

---
+++ Lab2/figures/Q.4/Heliplot3.m
@@ -0,0 +1,56 @@
+clc
+clear
+close
+Data = load('pitch2lqr.mat');
+Data2 = load('pitch3lqr.mat');
+Data3 = load('pitch4lqr.mat');
+Data4 = load('pitch6lqr.mat');
+Data5 = load('pitch5lqr.mat');
+
+figure(1);
+plot(Data.ans(1,:), Data.ans(2,:),'k')
+
+hold on
+
+plot(Data.ans(1,:), Data.ans(3,:),'b')
+
+hold on
+plot(Data2.ans(1,:), Data2.ans(3,:),'m')
+
+hold on
+plot(Data3.ans(1,:), Data3.ans(3,:),'c')
+
+hold on
+plot(Data4.ans(1,:), Data4.ans(3,:),'r')
+
+title('Pitch');
+legend('Step=0.3','Test 1','Test 2','Test 3','Test 4');
+hold off
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Data = load('elevationRate2lqr.mat');
+Data2 = load('elevationRate3lqr.mat');
+Data3 = load('elevationRate4lqr.mat');
+Data4 = load('elevationRate6lqr.mat');
+Data5 = load('elevationRate5lqr.mat');
+
+figure(2);
+plot(Data.ans(1,:), Data.ans(2,:),'k')
+
+hold on
+
+plot(Data.ans(1,:), Data.ans(3,:),'b')
+
+hold on
+plot(Data2.ans(1,:), Data2.ans(3,:),'m')
+
+hold on
+plot(Data3.ans(1,:), Data3.ans(3,:),'c')
+
+hold on
+plot(Data4.ans(1,:), Data4.ans(3,:),'r')
+xlim([14.9 16.6]);
+ylim([-0.2 0.2]);
+title('Elevation rate');
+legend('Step=0.1','Test 1','Test 2','Test 3','Test 4','Location','southeast');
+
---
+++ Lab2/heli_q8_quarc_win64/rtGetNaN.h
@@ -0,0 +1,30 @@
+/*
+ * rtGetNaN.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "heli_q8".
+ *
+ * Model version              : 1.78
+ * Simulink Coder version : 8.9 (R2015b) 13-Aug-2015
+ * C source code generated on : Wed Sep 30 16:02:51 2020
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_rtGetNaN_h_
+#define RTW_HEADER_rtGetNaN_h_
+#include <stddef.h>
+#include "rtwtypes.h"
+#include "rt_nonfinite.h"
+
+extern real_T rtGetNaN(void);
+extern real32_T rtGetNaNF(void);
+
+#endif                                 /* RTW_HEADER_rtGetNaN_h_ */
