620a6593933b8d176c52b35cb1b6a48abd901640
dr0nn1 <peda_95@hotmail.com>
2020-10-23 10:40:11 +0200

    finished lab 3

---
+++ Lab3/heli_q8_quarc_win64/heli_q8_dt.h
@@ -0,0 +1,162 @@
+/*
+ * heli_q8_dt.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "heli_q8".
+ *
+ * Model version              : 1.86
+ * Simulink Coder version : 8.9 (R2015b) 13-Aug-2015
+ * C source code generated on : Wed Oct 21 17:34:56 2020
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "ext_types.h"
+
+/* data type size table */
+static uint_T rtDataTypeSizes[] = {
+  sizeof(real_T),
+  sizeof(real32_T),
+  sizeof(int8_T),
+  sizeof(uint8_T),
+  sizeof(int16_T),
+  sizeof(uint16_T),
+  sizeof(int32_T),
+  sizeof(uint32_T),
+  sizeof(boolean_T),
+  sizeof(fcn_call_T),
+  sizeof(int_T),
+  sizeof(pointer_T),
+  sizeof(action_T),
+  2*sizeof(uint32_T),
+  sizeof(t_stream),
+  sizeof(t_stream_ptr),
+  sizeof(intmax_t),
+  sizeof(ptrdiff_t),
+  sizeof(size_t),
+  sizeof(t_int64),
+  sizeof(t_uint64),
+  sizeof(t_wide_char),
+  sizeof(t_game_controller),
+  sizeof(t_card),
+  sizeof(t_task)
+};
+
+/* data type name table */
+static const char_T * rtDataTypeNames[] = {
+  "real_T",
+  "real32_T",
+  "int8_T",
+  "uint8_T",
+  "int16_T",
+  "uint16_T",
+  "int32_T",
+  "uint32_T",
+  "boolean_T",
+  "fcn_call_T",
+  "int_T",
+  "pointer_T",
+  "action_T",
+  "timer_uint32_pair_T",
+  "t_stream",
+  "t_stream_ptr",
+  "intmax_t",
+  "ptrdiff_t",
+  "size_t",
+  "t_int64",
+  "t_uint64",
+  "t_wide_char",
+  "t_game_controller",
+  "t_card",
+  "t_task"
+};
+
+/* data type transitions for block I/O structure */
+static DataTypeTransition rtBTransitions[] = {
+  { (char_T *)(&heli_q8_B.Add), 0, 0, 50 },
+
+  { (char_T *)(&heli_q8_B.StreamCall1_o2), 3, 0, 1 },
+
+  { (char_T *)(&heli_q8_B.IfActionSubsystem2.In1), 0, 0, 1 },
+
+  { (char_T *)(&heli_q8_B.IfActionSubsystem.In1), 0, 0, 1 }
+  ,
+
+  { (char_T *)(&heli_q8_DW.HILInitialize_AIMinimums[0]), 0, 0, 78 },
+
+  { (char_T *)(&heli_q8_DW.StreamCall1_Stream), 14, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.GameController_Controller), 22, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.HILInitialize_Card), 23, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.HILReadEncoderTimebase_Task), 24, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.Elevation_PWORK.LoggedData[0]), 11, 0, 26 },
+
+  { (char_T *)(&heli_q8_DW.HILInitialize_ClockModes[0]), 6, 0, 46 },
+
+  { (char_T *)(&heli_q8_DW.HILInitialize_POSortedChans[0]), 7, 0, 8 },
+
+  { (char_T *)(&heli_q8_DW.ToFile_IWORK.Count), 10, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.If1_ActiveSubsystem), 2, 0, 5 },
+
+  { (char_T *)(&heli_q8_DW.StreamCall1_State), 3, 0, 1 },
+
+  { (char_T *)(&heli_q8_DW.IfActionSubsystem2.IfActionSubsystem_SubsysRanBC), 2,
+    0, 1 },
+
+  { (char_T *)(&heli_q8_DW.IfActionSubsystem3.IfActionSubsystem1_SubsysRanBC), 2,
+    0, 1 },
+
+  { (char_T *)(&heli_q8_DW.IfActionSubsystem.IfActionSubsystem_SubsysRanBC), 2,
+    0, 1 },
+
+  { (char_T *)(&heli_q8_DW.IfActionSubsystem1.IfActionSubsystem1_SubsysRanBC), 2,
+    0, 1 }
+};
+
+/* data type transition table for block I/O structure */
+static DataTypeTransitionTable rtBTransTable = {
+  19U,
+  rtBTransitions
+};
+
+/* data type transitions for Parameters structure */
+static DataTypeTransition rtPTransitions[] = {
+  { (char_T *)(&heli_q8_P.A1[0]), 0, 0, 119 },
+
+  { (char_T *)(&heli_q8_P.HILReadEncoderTimebase_clock), 6, 0, 10 },
+
+  { (char_T *)(&heli_q8_P.HILInitialize_analog_input_chan[0]), 7, 0, 39 },
+
+  { (char_T *)(&heli_q8_P.HILInitialize_active), 8, 0, 35 },
+
+  { (char_T *)(&heli_q8_P.ElevationCounttorad_Gain), 0, 0, 67 },
+
+  { (char_T *)(&heli_q8_P.StreamCall1_SendBufferSize), 6, 0, 2 },
+
+  { (char_T *)(&heli_q8_P.StreamFormattedWrite_MaxUnits), 7, 0, 1 },
+
+  { (char_T *)(&heli_q8_P.GameController_BufferSize), 5, 0, 1 },
+
+  { (char_T *)(&heli_q8_P.StringConstant_Value[0]), 3, 0, 258 },
+
+  { (char_T *)(&heli_q8_P.HILReadEncoderTimebase_Active), 8, 0, 5 }
+};
+
+/* data type transition table for Parameters structure */
+static DataTypeTransitionTable rtPTransTable = {
+  10U,
+  rtPTransitions
+};
+
+/* [EOF] heli_q8_dt.h */
---
+++ Lab3/heli_q8_quarc_win64/heli_q8_private.h
@@ -0,0 +1,44 @@
+/*
+ * heli_q8_private.h
+ *
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * Code generation for model "heli_q8".
+ *
+ * Model version              : 1.86
+ * Simulink Coder version : 8.9 (R2015b) 13-Aug-2015
+ * C source code generated on : Wed Oct 21 17:34:56 2020
+ *
+ * Target selection: quarc_win64.tlc
+ * Note: GRT includes extra infrastructure and instrumentation for prototyping
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_heli_q8_private_h_
+#define RTW_HEADER_heli_q8_private_h_
+#include "rtwtypes.h"
+#include "multiword_types.h"
+#include "zero_crossing_types.h"
+
+extern t_stream heli_q8_rtZt_stream;
+
+/* A global buffer for storing error messages (defined in quanser_common library) */
+EXTERN char _rt_error_message[512];
+int_T rt_WriteMat4FileHeader(FILE *fp,
+  int32_T m,
+  int32_T n,
+  const char_T *name);
+extern void heli_q8_IfActionSubsystem1(real_T rtu_In1, real_T *rty_Out1);
+extern void heli_q8_IfActionSubsystem(real_T rtu_In1,
+  B_IfActionSubsystem_heli_q8_T *localB);
+void heli_q8_output0(void);
+void heli_q8_update0(void);
+void heli_q8_output2(void);
+void heli_q8_update2(void);            /* private model entry point functions */
+extern void heli_q8_derivatives(void);
+
+#endif                                 /* RTW_HEADER_heli_q8_private_h_ */
